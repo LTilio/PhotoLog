bd8f2f1ca4ecda3399725899816ee0df8fc9c3e3
LTilio <leandro.tilio@gmail.com>
2025-01-08 19:47:04 -0300

    feat: db config e app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,29 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 
-const app = express()
+const app = express();
 
 //config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+//Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB connection
+require("./config/db.js");
+
+//routes
+const routes = require("./routes/Router.js");
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
